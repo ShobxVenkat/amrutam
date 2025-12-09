ed26a701c8bce933eed2fc49c6c7e4f04a9e6150
Shobhit Tiwari <officialshobhit108@gmail.com>
2025-12-09 17:40:43 +0530

    Fix filename case for Store.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Store from "./Pages/Store.jsx";
+
+import Store from "./pages/Store.jsx";
 import ProductDetails from "./pages/ProductDetails.jsx";
 import IngredientDetails from "./pages/IngredientDetails.jsx";
 import Forum from "./pages/Forum.jsx";
 
 import UserDashboard from "./pages/UserDashboard.jsx";
-
 
 export default function App() {
   return (
@@ -16,9 +15,8 @@ export default function App() {
         <Route path="/store" element={<Store />} />
         <Route path="/product/:slug" element={<ProductDetails />} />
         <Route path="/ingredient/:slug" element={<IngredientDetails />} />
-        <Route path="/forum" element={<Forum />} />  
-<Route path="/user-dashboard" element={<UserDashboard />} />
-
+        <Route path="/forum" element={<Forum />} />
+        <Route path="/user-dashboard" element={<UserDashboard />} />
       </Routes>
     </BrowserRouter>
   );
